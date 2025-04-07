4b5ff232007c6a3b32909e0629eaf220fda6b5c1
Steven Tran <steventran@Stevens-MacBook-Air.local>
2025-04-07 13:22:40 -0400

    Implement signout.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -15,6 +16,45 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: "Firebase User Authentication",
+      home: HomePage(title: "Firebase User Authentication")
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  void _signOut() async {
+    await auth.signOut();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text("User signed out"),
+      )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: _signOut
+          ),
+        ],
+      ),
+    );
   }
 }
